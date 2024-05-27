788247ef24a14e915e3eb87d1e4a0ef7dd3328db
vaneits <vacosta@itssolutions.co>
2024-05-27 18:08:13 -0500

    Tabla con historico de estimaciones HU 1

--- front-end-gpi/src/app/views/pages/estimacion-tiempos/estimaciones/estimaciones.component.ts
+++ front-end-gpi/src/app/views/pages/estimacion-tiempos/estimaciones/estimaciones.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 
 
 @Component({
@@ -11,10 +9,91 @@ import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
     templateUrl: './estimaciones.component.html',
     styleUrls: ['./estimaciones.component.scss']
 })
-export class Estimaciones_Iseries implements OnInit {
+export class Estimaciones_Iseries  implements OnInit {
     ngOnInit(): void {
         throw new Error('Method not implemented.');
     }
+    displayedColumns: string[] = ['id', 'name', 'progress', 'client'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
 
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
 
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    client: CLIENTS[Math.round(Math.random() * (CLIENTS.length - 1))],
+  };
+}
+
+export interface UserData {
+    id: string;
+    name: string;
+    progress: string;
+    client: string;
+  }
+  
+  /** Constants used to fill up our data base. */
+  const CLIENTS: string[] = [
+    'blueberry',
+    'lychee',
+    'kiwi',
+    'mango',
+    'peach',
+    'lime',
+    'pomegranate',
+    'pineapple',
+  ];
+  const NAMES: string[] = [
+    'Maia',
+    'Asher',
+    'Olivia',
+    'Atticus',
+    'Amelia',
+    'Jack',
+    'Charlotte',
+    'Theodore',
+    'Isla',
+    'Oliver',
+    'Isabella',
+    'Jasper',
+    'Cora',
+    'Levi',
+    'Violet',
+    'Arthur',
+    'Mia',
+    'Thomas',
+    'Elizabeth',
+  ]
