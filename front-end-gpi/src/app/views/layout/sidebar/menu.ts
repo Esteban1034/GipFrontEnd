8d6ee71819dcdb585b7c2e4e095a6892aa96bb97
Luis Fernando Vergara Gacharna <lvergara@itssolutions.co>
2024-06-04 10:55:16 -0500

    se agrega el front para crear estimaciones

--- front-end-gpi/src/app/views/layout/sidebar/menu.ts
+++ front-end-gpi/src/app/views/layout/sidebar/menu.ts
@@ -236,6 +236,10 @@ export const MENU_DP: MenuItem[] = [
                         link: '/estimacion tiempos/estimaciones'
                     },
                     {
+                        label: 'Crear estimación',
+                        link: '/estimacion tiempos/crear-estimacion'
+                    },
+                    {
                         label: 'Mantenimiento Peso Hora',
                         link: '/estimacion tiempos/mant-peso-hora'
                     },
