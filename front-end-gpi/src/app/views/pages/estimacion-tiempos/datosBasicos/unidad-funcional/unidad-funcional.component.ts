bd3f9e8a53424f301cc6ac54383796884e09eedd
vaneits <vacosta@itssolutions.co>
2024-06-07 09:20:28 -0500

    Se ubica carpeta unidad-funcional dentro de estimacion tiempo

---
+++ front-end-gpi/src/app/views/pages/estimacion-tiempos/datosBasicos/unidad-funcional/unidad-funcional.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+
+
+@Component({
+    selector: "app-unidad-funcional",
+    templateUrl: "./unidad-funcional.component.html",
+    styleUrls: ["./unidad-funcional.component.scss"],})
+
+
+  
