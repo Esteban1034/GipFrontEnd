626e764df18df2fb8f2b2e6d9d8111ef064d784f
SimonL <simonlopess16@gmail.com>
2024-06-18 12:20:31 -0500

    modelo service

--- front-end-gpi/src/app/service/modelo.service.ts
+++ front-end-gpi/src/app/service/modelo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Modelo } from '../Model/modelo';
+import { Modelo } from '../model/modelo';
 import { HttpHeaderApp } from './header';
 
 @Injectable({
